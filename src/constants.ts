c99115e0c4641c80b61c244fd10b3dc915b9ec17
tonythornton435 <thorntont435@gmail.com>
2022-05-21 07:16:28 +0300

    Fix constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,1 @@
-// export const INDEX_API_BASE_URL = "http://134.122.87.44:8000/api/";
-
-export const INDEX_API_BASE_URL = "http://192.168.0.131:8000/api/";
+export const INDEX_API_BASE_URL = "http://134.122.87.44:8000/api/";
